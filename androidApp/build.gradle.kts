6ec7c2356074ec9cb713b537ee84549ee34f516d
Teodor Ganga <teodorionut.ganga@endava.com>
2023-02-02 17:39:00 +0200

    Added MainFlowActivity with UI; Added custom theme; Versions update

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -33,10 +33,14 @@ android {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
-    implementation("androidx.compose.foundation:foundation:1.2.1")
-    implementation("androidx.compose.material:material:1.2.1")
-    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.compose.ui:ui:1.3.3")
+    implementation("androidx.compose.ui:ui-tooling:1.3.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.3")
+    implementation("androidx.compose.foundation:foundation:1.3.1")
+    implementation("androidx.compose.material:material:1.3.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 }
--- androidApp/src/main/java/com/bubblego/app/android/theme/BubbleCarTheme.kt
+++ androidApp/src/main/java/com/bubblego/app/android/theme/BubbleCarTheme.kt
@@ -1,49 +1,59 @@
-package com.bubblego.app.android
+package com.bubblego.app.android.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
 import androidx.compose.material.Typography
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bubblego.app.android.R
 
 @Composable
-fun MyApplicationTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+fun BubbleCarTheme(
+    //darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        darkColors(
-            primary = Color(0xFFBB86FC),
-            primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
-        )
-    } else {
-        lightColors(
-            primary = Color(0xFF6200EE),
-            primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
-        )
-    }
+    val colors = lightColors(
+        primary = Color(0xFF3662FF),
+        primaryVariant = Color(0x213662FF),
+        secondary = Color(0xFFD9D9D9),
+        background = Color(0xFFFFFFFF),
+        onSurface = Color(0xFF000000),
+    )
+
     val typography = Typography(
+        h1 = TextStyle(
+            fontFamily = FontFamily(Font(R.font.airbnb_cereal_w_bd)),
+            fontWeight = FontWeight.Normal,
+            fontSize = 30.sp
+        ),
+        h2 = TextStyle(
+            fontFamily = FontFamily(Font(R.font.airbnb_cereal_w_md)),
+            fontWeight = FontWeight.Normal,
+            fontSize = 20.sp
+        ),
         body1 = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
             fontSize = 16.sp
+        ),
+        button = TextStyle(
+            fontFamily = FontFamily(Font(R.font.airbnb_cereal_w_md)),
+            fontWeight = FontWeight.Normal,
+            fontSize = 20.sp
         )
     )
     val shapes = Shapes(
         small = RoundedCornerShape(4.dp),
         medium = RoundedCornerShape(4.dp),
-        large = RoundedCornerShape(0.dp)
+        large = RoundedCornerShape(36.dp)
     )
 
     MaterialTheme(
